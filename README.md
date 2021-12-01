e324f741fd7aa61323da83b8fba9c0442d3c850b
Tristan <git@tristan.rs>
2021-12-01 17:38:13 +0100

    Solved day 01

---
+++ README.md
@@ -0,0 +1,14 @@
+https://adventofcode.com/2021/
+
+# Running each day
+
+```
+cargo run --bin day_01
+# replacing day_01 with the day to run
+```
+
+# Running test cases
+```
+cargo test --bin day_01 -- --nocapture
+# -- --nocapture only needed when debugging with print outs
+```
---
+++ src/day_01.rs
@@ -0,0 +1,47 @@
+fn part1(input: &[usize]) -> usize {
+    input.windows(2)
+        .filter(|arr| arr[0] < arr[1])
+        .count()
+}
+
+fn part2(input: &[usize]) -> usize {
+    input.windows(3)
+        .map(|arr| arr[0] + arr[1] + arr[2])
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|arr| arr[0] < arr[1])
+        .count()
+}
+
+fn main() {
+    let input = include_str!("../day_01_input.txt")
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| line.parse::<usize>())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>();
+    println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
+}
+
+#[test]
+mod test {
+
+    use super::{part1, part2};
+
+    #[test]
+    fn test_part1() {
+        let input = vec![
+            199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+        ];
+        assert_eq!(part1(&input), 7)
+    }
+    #[test]
+    fn test_part2() {
+        let input = vec![
+            199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+        ];
+        assert_eq!(part2(&input), 5)
+    }
+
+}
